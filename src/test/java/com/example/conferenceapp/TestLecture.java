1cb0e7dbb4bff2487024d24b2552b4e2a3687fd5
poxnora <kwojcik99@gmail.com>
2022-06-11 20:13:12 +0200

    tests

---
+++ src/test/java/com/example/conferenceapp/TestLecture.java
@@ -0,0 +1,79 @@
+package com.example.conferenceapp;
+
+import com.example.conferenceapp.model.Lecture;
+import com.example.conferenceapp.model.User;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestLecture extends TestAbstract{
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Test()
+    public void getLecturesList() throws Exception {
+        String uri = "/lectures/";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(500, status);
+
+    }
+
+    @Test
+    public void addLectures() throws Exception {
+        String uri = "/users/add";
+        Lecture lecture = new Lecture();
+        lecture.setTitle("lecture");
+        lecture.setTheme(1);
+        lecture.setStarts(1);
+
+        String inputJson = super.mapToJson(lecture);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(500, status);
+
+    }
+
+    @Test
+    public void updateLectures() throws Exception {
+        String uri = "/users/add/2";
+        Lecture lecture = new Lecture();
+        lecture.setTitle("lecture");
+        lecture.setTheme(1);
+        lecture.setStarts(1);
+
+        String inputJson = super.mapToJson(lecture);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.put(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(500, status);
+
+    }
+
+    @Test
+    public void deleteLectures() throws Exception {
+        String uri = "/users/delete/2";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(content, "Deleted successfully");
+    }
+}
+
+
